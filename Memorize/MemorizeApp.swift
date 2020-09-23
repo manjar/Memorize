8b19907bc3d65ceeda9c5fb322845eef55f615d8
Eli Manjarrez <manjarrez@apple.com>
2020-09-23 13:59:10 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eli Manjarrez on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

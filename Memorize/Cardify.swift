08ff095fc6848013c75cf7f67efb246dae7073cf
Eli Manjarrez <manjarrez@apple.com>
2020-11-04 13:20:45 -0800

    Starting to build out main classes

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -7,18 +7,32 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    var rotation: Double
+    var isFaceUp: Bool {
+        rotation < 90.0
+    }
+    
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0.0 : 180.0
+    }
+    
     func body(content: Content) -> some View {
         ZStack {
-            if (isFaceUp) {
+            Group {
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: lineWidth)
                 RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                 content
-            } else {
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
-            }
+            }.opacity(isFaceUp ? 1.0 : 0.0)
+            RoundedRectangle(cornerRadius: cornerRadius).fill().opacity(isFaceUp ? 0.0 : 1.0)
         }
+        .rotation3DEffect(Angle(degrees: rotation), axis: (x: 0.0, y: 1.0, z: 0.0)
+            )
     }
     
     private let cornerRadius: CGFloat = 10.0
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,24 +12,28 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         VStack {
-            Button(action: {
-                viewModel.newMemoryGame()
-            }, label: {
-                Text("New game")
-            })
             Text("Score: \(viewModel.score)")
             Grid(viewModel.cards) { card in
                 CardView(card: card)
                     .onTapGesture() {
-                        viewModel.choose(card: card)
+                        withAnimation(.linear) {
+                            viewModel.choose(card: card)
+                        }
                     }
                     .aspectRatio(contentMode:.fit)
                     .padding(5.0)
             }
             .padding()
             .foregroundColor(EmojiMemoryGame.themeColor)
+            Button(action: {
+                withAnimation(.easeInOut) {
+                    viewModel.newMemoryGame()
+                }
+            }, label: {
+                Text("New Game")
+            })
             Text("Theme: \(EmojiMemoryGame.themeName)")
-        }
+        }.font(Font.system(size: 32))
     }
 }
 
@@ -42,16 +46,35 @@ struct CardView: View {
         }
     }
     
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration:card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size:CGSize) -> some View {
         if (card.isFaceUp || !card.isMatched) {
             ZStack {
-                Pie(startAngle: Angle(degrees: 0.0 - 90.0), endAngle: Angle(degrees: 110 - 90.0), clockwise: true).padding(5.0).opacity(0.4)
+                Group {
+                    if (card.isConsumingBonusTime) {
+                        Pie(startAngle: Angle(degrees: 0.0 - 90.0), endAngle: Angle(degrees: -(animatedBonusRemaining * 360.0) - 90.0), clockwise: true)
+                            .onAppear {
+                                self.startBonusTimeAnimation()
+                            }
+                    } else {
+                        Pie(startAngle: Angle(degrees: 0.0 - 90.0), endAngle: Angle(degrees: -(card.bonusRemaining * 360.0) - 90.0), clockwise: true)
+                    }
+                }.padding(5.0).opacity(0.4)
                 Text(card.content)
                     .font(Font.system(size: fontSize(for: size)))
                     .rotationEffect(Angle(degrees: card.isMatched ? 360.0 : 0.0))
-                    .animation(card.isMatched ? Animation.linear(duration: 1.0).repeatForever(autoreverses: false) : .default)
+                    .animation(card.isMatched ? Animation.linear.repeatForever(autoreverses: false) : .default)
             }.cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         } else {
             EmptyView()
         }
